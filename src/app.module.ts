3e9281912da9c515ae431cd30c5a0301eaa1adba
yaroslav-volkov-dev <volkov.yaroslav94@gmail.com>
2024-12-12 00:53:14 +0200

    added product service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SupabaseModule } from './supabase/supabase.module';
+import { SupabaseService } from './supabase/supabase.service';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SupabaseModule,
+    ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SupabaseService],
 })
 export class AppModule {}
