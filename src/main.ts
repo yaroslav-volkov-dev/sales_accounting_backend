cf0e62470bad671d8be236b1edd31138fc30a45a
yaroslav-volkov-dev <volkov.yaroslav94@gmail.com>
2024-12-12 22:08:15 +0200

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 
